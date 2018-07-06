78c7b92ca294a1b365c25e408b2d3a666ce0876c
kHRYSTAL <723526676@qq.com>
2018-07-06 11:02:41 +0800

    feat[dao.split#master/slave]:add master-slave branch, update interceptor and mybatis config and spring-dao config

--- src/main/java/me/khrystal/market/dao/split/DynamicDataSourceInterceptor.java
+++ src/main/java/me/khrystal/market/dao/split/DynamicDataSourceInterceptor.java
@@ -6,9 +6,11 @@ import org.apache.ibatis.executor.keygen.SelectKeyGenerator;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.Locale;
@@ -17,16 +19,23 @@ import java.util.Properties;
 /**
  * Created by kHRYSTAL on 18/7/5.
  */
+@Intercepts({@Signature(type = Executor.class, method = "update", args={MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query", args={MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
 public class DynamicDataSourceInterceptor implements Interceptor {
+
+    private static Logger logger = LoggerFactory.getLogger(DynamicDataSourceInterceptor.class);
+
+    // u0020 表示空格
+    private static final String REGEX = ".*insert\\u0020.*|.*delete\\u0020.*|.*update\\u0020.*";
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
+        String lookupKey = DynamicDataSourceHolder.DB_MASTER;
         // 判断当前是不是事务
         boolean synchoronizationActive = TransactionSynchronizationManager.isActualTransactionActive();
-        if (!synchoronizationActive) {
-            Object[] objects = invocation.getArgs();
-            MappedStatement ms = (MappedStatement) objects[0];
-            String lookupKey;
+        Object[] objects = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) objects[0];
+        if (!synchoronizationActive) { // 不是事务 按照sql语句去区分
             // 判断是否是读方法
             if (ms.getSqlCommandType().equals(SqlCommandType.SELECT)) {
                 // 且selectKey 为自增id查询主键(SELECT LAST_INSERT_ID()) 方法 使用主库
@@ -37,11 +46,21 @@ public class DynamicDataSourceInterceptor implements Interceptor {
                     BoundSql boundSql = ms.getSqlSource().getBoundSql(objects[1]);
                     // 将制表符 回车都替换为空格
                     String sql = boundSql.getSql().toLowerCase(Locale.CHINA).replaceAll("[\\t\\n\\r]", " ");
-                    // TODO: 18/7/5  
+
+                    if (sql.matches(REGEX)) { // 写操作使用主库
+                        lookupKey = DynamicDataSourceHolder.DB_MASTER;
+                    } else { // 读操作使用从库
+                        lookupKey = DynamicDataSourceHolder.DB_SLAVE;
+                    }
                 }
             }
+        } else { // 事务操作 都是写操作 使用主库
+            lookupKey = DynamicDataSourceHolder.DB_MASTER;
         }
-        return null;
+        logger.debug("设置方法[{}] use [{}] Strategy, SqlCommanType[{}]..", ms.getId(), lookupKey, ms.getSqlCommandType().name());
+        DynamicDataSourceHolder.setDbType(lookupKey);
+        // 拦截器处理完毕 执行后续流程
+        return invocation.proceed();
     }
 
     @Override
